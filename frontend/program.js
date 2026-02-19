d12811b6134a54133d3841ecf4a968d9f22abc72
Dylan Pineda <dyspinedaa@gmail.com>
2026-02-18 20:02:04 -0500

    v0.3 frontend básico

---
+++ frontend/program.js
@@ -0,0 +1,16 @@
+myButton = document.getElementById("btn_enviar")
+
+myButton.addEventListener(
+    
+    "click", async function (){
+
+        let response = await fetch("http://127.0.0.1:8000")
+        let data = await response.json();
+
+        myResult = document.getElementById("txt_resultado")
+
+        myresult.innerText = data;
+
+
+    }
+)
